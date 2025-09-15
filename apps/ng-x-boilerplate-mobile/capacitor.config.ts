c5ee1e132edf20a101b251d9c975ebcfe62eedc5
Ali <imalitavakoli@users.noreply.github.com>
2025-09-15 12:18:26 +0200

    x/apps/x-boilerplate-mobile: updated native projects

--- apps/ng-x-boilerplate-mobile/capacitor.config.ts
+++ apps/ng-x-boilerplate-mobile/capacitor.config.ts
@@ -1,20 +1,33 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import { KeyboardResize, KeyboardStyle } from '@capacitor/keyboard';
 
 const config: CapacitorConfig = {
   appId: 'com.x.xxx',
   appName: 'X',
   webDir: '../../fin/apps/ng-x-boilerplate-mobile/browser',
-  bundledWebRuntime: false,
   server: {
     androidScheme: 'https',
   },
 
   plugins: {
-    /* Push Notifications /////////////////////////////////////////////////// */
+    // Push Notifications
     PushNotifications: {
       presentationOptions: ['badge', 'sound', 'alert'],
     },
-    /* Bypass Azure Cors //////////////////////////////////////////////////// */
+
+    // https://github.com/capawesome-team/capacitor-plugins/tree/main/packages/android-edge-to-edge-support
+    EdgeToEdge: {
+      backgroundColor: '#ffffff',
+    },
+
+    // https://capacitorjs.com/docs/apis/keyboard
+    Keyboard: {
+      resize: KeyboardResize.Native,
+      style: KeyboardStyle.Default,
+      resizeOnFullScreen: false, // Why false? To make it work smoothly with `EdgeToEdge`. Otherwise, the web view will be resized to fit the screen.
+    },
+
+    // Bypass Azure Cors
     CapacitorHttp: {
       enabled: true,
     },
