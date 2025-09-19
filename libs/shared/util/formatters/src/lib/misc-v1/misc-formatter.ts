04af982b000050b4bd9e14ebe2870e1fc6875a20
Ali <imalitavakoli@users.noreply.github.com>
2025-09-19 15:48:17 +0200

    Update misc-formatter.ts

--- libs/shared/util/formatters/src/lib/misc-v1/misc-formatter.ts
+++ libs/shared/util/formatters/src/lib/misc-v1/misc-formatter.ts
@@ -27,3 +27,34 @@ export function v1MiscAddAlreadyMappedProps(data: any, map: any): any {
 export function v1MiscFixPath(input: string) {
   return input.replace(/(?:\/|\s)+$/, '');
 }
+
+/**
+ * Make a string like `/dashboard` compatible with GA4 (Google Analytics 4)
+ * naming convention. We basically do the following:
+ * - Remove the last trailing slash.
+ * - Replace the first trailing slash with 'navTo_'.
+ * - Replace any other slashes with '_'.
+ * - Replace '-' with CamelCase. e.g., `my-dashboard` becomes `myDashboard`.
+ * - Remove '?' (if it exists) and everything that comes after it.
+ * - If character length is more than 40, then truncate it to 40 characters.
+ *
+ * So as an example `/my-dashboard` becomes `navTo_myDashboard`,
+ * `/my-dashboard/` becomes `navTo_myDashboard`, `/my-dashboard/overview`
+ * becomes `navTo_myDashboard_overview`, and
+ * `/my-dashboard/overview/very-long-name-is-here` becomes
+ * `navTo_myDashboard_overview_veryLongNameI`.
+ *
+ * @export
+ * @param {string} input
+ * @returns {string}
+ */
+export function v1MiscMakeGA4ForPageNav(input: string) {
+  let output = input.trim();
+  if (output.endsWith('/')) output = output.slice(0, -1);
+  if (output.startsWith('/')) output = 'navTo_' + output.slice(1);
+  output = output.replace(/\//g, '_');
+  output = output.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+  if (output.includes('?')) output = output.split('?')[0];
+  if (output.length > 40) output = output.slice(0, 40);
+  return output;
+}
