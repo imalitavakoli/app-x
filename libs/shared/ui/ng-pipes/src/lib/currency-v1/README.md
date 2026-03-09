309340de10f655e2cf712df7927278621e5d40ec
Ali <imalitavakoli@users.noreply.github.com>
2026-03-09 12:31:28 +0100

    x/libs/ui/pipes/updated

--- libs/shared/ui/ng-pipes/src/lib/currency-v1/README.md
+++ libs/shared/ui/ng-pipes/src/lib/currency-v1/README.md
@@ -4,7 +4,14 @@ v1.
 
 ## Implementation guide
 
-The Angular's default currency pipe doesn't support some currencies, so this pipe has been created to handle those cases.
+The Angular's default currency pipe doesn't support some currencies, so this pipe has been created to handle those cases + adding some custom logic.
+
+**What pipe's `truncate` argument does?**
+When `truncate` is `true`, in simple terms, we first first cut off extra decimals with `Math.trunc`, and then call `toLocaleString` to format the number... So after truncation, there's nothing left for `toLocaleString` to round up (because if we give a number with more decimals than `fractionDigits` to `toLocaleString`, it will round up the number as well).  
+For example, if `value` is `1.235` and `fractionDigits` is `2`, then:
+
+- When `truncate` is `false`, `toLocaleString` will round up `1.235` to `1.24`.
+- When `truncate` is `true`, we first truncate `1.235` to `1.23`, and then `toLocaleString` will keep it as `1.23`.
 
 ```ts
 import { Component, inject } from '@angular/core';
@@ -25,7 +32,7 @@ export class TestComponent {}
 ```
 
 ```html
-{{ 20.45 | currencyV1: 'SEK' : 'sv-SE' }} // 20,45 kr
+{{ 20.45 | currencyV1: 'SEK' : 'sv-SE' : false }} // 20,45 kr
 ```
 
 ## Running unit tests
--- libs/shared/ui/ng-pipes/src/lib/currency-v1/currency.pipe.ts
+++ libs/shared/ui/ng-pipes/src/lib/currency-v1/currency.pipe.ts
@@ -10,7 +10,12 @@ import {
   standalone: true,
 })
 export class V1CurrencyPipe implements PipeTransform {
-  transform(value: number, currencyCode = 'USD', locale = 'en-GB'): string {
+  transform(
+    value: number,
+    currencyCode = 'USD',
+    locale = 'en-GB',
+    truncate = false,
+  ): string {
     // Format `value` by Angular function.
     // NOTE: We don't use this method, because it doesn't support some languages.
     // const digitsInfo = v1NumberGetDigitsInfo(value, 'cost');
@@ -24,7 +29,20 @@ export class V1CurrencyPipe implements PipeTransform {
 
     // Format `value` by vanilla JS function.
     const fractionDigits = v1NumberGetFractionDigits(value, 'cost');
-    return value.toLocaleString(locale, {
+    let formattedValue = value;
+
+    // When `truncate` is `true`, in simple terms, we first first cut off extra
+    // decimals with `Math.trunc`, and then call `toLocaleString` to format the
+    // number... So after truncation, there's nothing left for `toLocaleString`
+    // to round up (because if we give a number with more decimals than
+    // `fractionDigits` to `toLocaleString`, it will round up the number as
+    // well).
+    if (truncate) {
+      const factor = 10 ** fractionDigits;
+      formattedValue = Math.trunc(value * factor) / factor;
+    }
+
+    return formattedValue.toLocaleString(locale, {
       style: 'currency',
       currencyDisplay: 'narrowSymbol',
       currency: currencyCode,
